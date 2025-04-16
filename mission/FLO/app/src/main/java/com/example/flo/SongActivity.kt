9348bcf4057be850193ba9eca792bc47d6f9c5c4
tama <rufina130@naver.com>
2025-04-16 19:32:12 +0900

    mission: 4주차 미션 인증

--- mission/FLO/app/src/main/java/com/example/flo/SongActivity.kt
+++ mission/FLO/app/src/main/java/com/example/flo/SongActivity.kt
@@ -5,10 +5,14 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySongBinding
 
+//코틀린에서의 상속은 : 을 사용한다.
 class SongActivity : AppCompatActivity() {
 
+    //전방선언. 선언은 지금할 건데, 나중에 초기화를 해줄게 (>초기화)
+    //val 은 후에 값의 수정 불가
     lateinit var binding : ActivitySongBinding
 
+    //무조건 실행되어야하는 함수
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
